fee2f3772527e9591c716e14317966008a318ad8
M Selvakumar <selva.developer@gmail.com>
2018-02-11 23:14:43 +0530

    names DataFeed and MultiplexedDataFeed takes in datafeeds and provides permutated batches in return - Voila!!

--- datafeed.py
+++ datafeed.py
@@ -4,12 +4,15 @@ from logger import datafeed_logger
 log = datafeed_logger.getLogger('main')
 log.setLevel(Config.Log.DATAFEED.level)
 
+import random
+
 from utilz import tqdm
 from debug import memory_consumed
 
 class DataFeed(object):
 
-    def __init__(self,  datapoints, batchop, batch_size=1, vocab=None, sort_key=None):
+    def __init__(self, name, datapoints, batchop, batch_size=1, vocab=None, sort_key=None):
+        self.name        = name
         self._offset     = 0
         self._size       = len(datapoints)
         self._batch_size = batch_size
@@ -66,18 +69,24 @@ class DataFeed(object):
     def offset(self):
         return self._offset
     
-    def batch(self, apply_batchop=True):
-        self._offset += self.batch_size
-        b = self.data[ self.offset - self.batch_size   :   self.offset ]
+    def batch(self, batch_size=None, apply_batchop=True):
+        if not batch_size:
+            batch_size = self.batch_size
+            
+        self._offset += batch_size
+        b = self.data[ self.offset - batch_size   :   self.offset ]
         if apply_batchop:
             return self._batchop(b, self.vocab)
         return b
     
-    def next_batch(self, apply_batchop=True):
+    def next_batch(self, batch_size=None, apply_batchop=True):
         try:
-            if self.offset + self.batch_size > self.size:
+            if not batch_size:
+                batch_size = self.batch_size
+                
+            if self.offset + batch_size > self.size:
                 self.reset_offset()
-            return self.batch(apply_batchop=apply_batchop)
+            return self.batch(batch_size=batch_size, apply_batchop=apply_batchop)
         except:
             log.exception('batch failed')
             return self.next_batch(apply_batchop=apply_batchop)
@@ -91,3 +100,99 @@ class DataFeed(object):
 
     def reset_offset(self):
         self._offset = 0
+
+
+class MultiplexedDataFeed(DataFeed):
+    
+    def __init__(self, name, datafeeds, batchop, batch_size=1, vocab=None, sort_key=None):
+        self.name        = name
+        self._offset     = 0
+        self._size       = sum([feed.size for feed in datafeeds])
+        self._batch_size = batch_size
+        self._batchop    = batchop
+        self.vocab = vocab
+        self._batch_cache = {}
+
+        self.bind(datafeeds)
+        
+        log.info('built Datafeed with the following props:')
+        log.info(' size       : {}'.format(self.size))
+        log.info(' batch_size : {}'.format(self.batch_size))
+        log.info(' num_batch  : {}'.format(self.num_batch))
+        
+
+    def bind(self, datafeeds):
+        self.datafeeds = datafeeds
+        self._data_dict = {}
+
+        if self.size > self.batch_size * self.num_batch:
+            log.info('batch bleeds')
+            self._size += self.batch_size
+            
+        self.reset_offset()
+
+        for datapoints in self.datafeeds:
+            for d in datapoints.data:
+                self._data_dict[d.id] = d
+
+
+    @property
+    def data_dict(self):
+        return self._data_dict
+    
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def batch_size(self):
+        return self._batch_size
+
+    @property
+    def num_batch(self):
+        return int(self.size/self.batch_size)
+    
+    @property
+    def offset(self):
+        return self._offset
+    
+    def batch(self, batch_size=None, apply_batchop=True):
+        if not batch_size:
+            batch_size = self.batch_size
+
+        b = []
+        for feed in self.datafeeds:
+            b.extend(feed.next_batch(batch_size=1, apply_batchop=False))
+            if len(b) == batch_size: break
+            
+        self._offset += batch_size
+        
+        if apply_batchop:
+            return self._batchop(b, self.vocab)
+        return b
+
+    '''
+    def next_batch(self, batch_size=None, apply_batchop=True):
+        try:
+            if not batch_size:
+                batch_size = self.batch_size
+                
+            if self.offset + batch_size > self.size:
+                self.reset_offset()
+            return self.batch(batch_size=batch_size, apply_batchop=apply_batchop)
+        except:
+            log.exception('batch failed')
+            return self.next_batch(apply_batchop=apply_batchop)
+    '''
+    
+    def nth_batch(self, n, apply_batchop=True):
+        b = []
+        for feed in self.datafeeds:
+            b.append(random.choice(feed.nth_batch(min(n, random.choice(range(feed.num_batch))), apply_batchop=False)))
+            if len(b) == self.batch_size: break
+                    
+        if apply_batchop:
+            return self._batchop(b, self.vocab)
+        return b
+
+
